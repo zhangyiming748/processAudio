95acd20545ec2e4261f39ceb8ebf538b32ec7c69
zen <zhangyiming748@gmail.com>
2023-01-27 16:21:41 +0800

    转码声音提醒

--- ProcessAudio.go
+++ ProcessAudio.go
@@ -5,12 +5,24 @@ import (
 	"github.com/zhangyiming748/log"
 	"github.com/zhangyiming748/processAudio/convert"
 	"github.com/zhangyiming748/processAudio/util"
+	"github.com/zhangyiming748/voiceAlert"
 	"os"
 	"strings"
 	"time"
 )
 
+const (
+	success  = iota + 1 // 单次转码成功
+	failed              // 转码失败,程序退出
+	complete            // 转码进程完成
+)
+
 func ProcessAudio(dir, pattern string) {
+	defer func() {
+		if err := recover(); err != nil {
+			voiceAlert.Voice(failed)
+		}
+	}()
 	m_start := time.Now()
 	start := time.Now().Format("整个任务开始时间 15:04:03")
 	log.Debug.Println(start)
@@ -19,14 +31,21 @@ func ProcessAudio(dir, pattern string) {
 	files = util.GetMultiFiles(dir, pattern)
 	for _, file := range files {
 		convert.Convert2AAC(file)
+		voiceAlert.Voice(success)
 	}
 	m_end := time.Now()
 	end := time.Now().Format("整个任务结束时间 15:04:03")
 	log.Debug.Println(end)
 	during := m_end.Sub(m_start).Minutes()
 	log.Debug.Printf("整个任务用时 %v 分\n", during)
+	voiceAlert.Voice(complete)
 }
 func ProcessAllAudio(root, pattern string) {
+	defer func() {
+		if err := recover(); err != nil {
+			voiceAlert.Voice(failed)
+		}
+	}()
 	m_start := time.Now()
 	start := time.Now().Format("整个任务开始时间 15:04:03")
 	log.Debug.Println(start)
@@ -36,12 +55,14 @@ func ProcessAllAudio(root, pattern string) {
 		files = util.GetMultiFiles(src, pattern)
 		for _, file := range files {
 			convert.Convert2AAC(file)
+			voiceAlert.Voice(success)
 		}
 	}
 	m_end := time.Now()
 	end := time.Now().Format("整个任务结束时间 15:04:03")
 	log.Debug.Println(end)
 	during := m_end.Sub(m_start).Minutes()
+	voiceAlert.Voice(complete)
 	log.Debug.Printf("整个任务用时 %v 分\n", during)
 }
 
--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.19
 require (
 	github.com/zhangyiming748/log v0.0.3
 	github.com/zhangyiming748/replace v0.0.4
+	github.com/zhangyiming748/voiceAlert v0.0.9
 )
