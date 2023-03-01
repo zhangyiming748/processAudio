390cc5fbcd5773f11d6587d4704477662ffdfba1
zen <zhangyiming748@gmail.com>
2023-03-01 18:39:28 +0800

    使用简短语音提示提高效率

--- ProcessAudio.go
+++ ProcessAudio.go
@@ -3,55 +3,31 @@ package processAudio
 import (
 	"github.com/zhangyiming748/GetAllFolder"
 	"github.com/zhangyiming748/GetFileInfo"
-	"github.com/zhangyiming748/log"
 	"github.com/zhangyiming748/processAudio/convert"
 	"github.com/zhangyiming748/voiceAlert"
-	"time"
 )
 
 func ProcessAudio(dir, pattern string) {
 	defer func() {
 		if err := recover(); err != nil {
-			voiceAlert.Voice(voiceAlert.FAILED)
+			voiceAlert.Customize("failed", voiceAlert.Samantha)
 		}
 	}()
-	m_start := time.Now()
-	start := time.Now().Format("整个任务开始时间 15:04:03")
-	log.Debug.Println(start)
 	files := GetFileInfo.GetAllFileInfo(dir, pattern)
+	if len(files) == 0 {
+		voiceAlert.Customize("skip", voiceAlert.Samantha)
+	}
 	for _, file := range files {
 		convert.Convert2AAC(file)
-		voiceAlert.Voice(voiceAlert.SUCCESS)
+		voiceAlert.Customize("done", voiceAlert.Samantha)
 	}
-	m_end := time.Now()
-	end := time.Now().Format("整个任务结束时间 15:04:03")
-	log.Debug.Println(end)
-	during := m_end.Sub(m_start).Minutes()
-	log.Debug.Printf("整个任务用时 %v 分\n", during)
-	voiceAlert.Voice(voiceAlert.COMPLETE)
+	voiceAlert.Customize("complete", voiceAlert.Samantha)
 }
+
 func ProcessAllAudio(root, pattern string) {
-	defer func() {
-		if err := recover(); err != nil {
-			voiceAlert.Voice(voiceAlert.FAILED)
-		}
-	}()
-	m_start := time.Now()
-	start := time.Now().Format("整个任务开始时间 15:04:03")
-	log.Debug.Println(start)
 	ProcessAudio(root, pattern)
 	folders := GetAllFolder.ListFolders(root)
-	for _, dir := range folders {
-		files := GetFileInfo.GetAllFileInfo(dir, pattern)
-		for _, file := range files {
-			convert.Convert2AAC(file)
-			voiceAlert.Voice(voiceAlert.SUCCESS)
-		}
+	for _, folder := range folders {
+		ProcessAudio(folder, pattern)
 	}
-	m_end := time.Now()
-	end := time.Now().Format("整个任务结束时间 15:04:03")
-	log.Debug.Println(end)
-	during := m_end.Sub(m_start).Minutes()
-	voiceAlert.Voice(voiceAlert.COMPLETE)
-	log.Debug.Printf("整个任务用时 %v 分\n", during)
 }
--- go.mod
+++ go.mod
@@ -3,9 +3,9 @@ module github.com/zhangyiming748/processAudio
 go 1.19
 
 require (
-	github.com/zhangyiming748/log v0.0.3
+	github.com/zhangyiming748/log v0.0.5
 	github.com/zhangyiming748/replace v0.0.4
-	github.com/zhangyiming748/voiceAlert v0.0.10
+	github.com/zhangyiming748/voiceAlert v0.0.12
 )
 
 require github.com/zhangyiming748/GetAllFolder v0.0.2
